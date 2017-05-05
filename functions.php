4599bde5c4398759f5fa383311114ac94da94795
Brandyn Burbank <brandyn@brandynburbank.com>
2017-05-05 14:08:34 -0600

    fixed enqueue styles error

--- functions.php
+++ functions.php
@@ -2,8 +2,7 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'montserrat', 'https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i,900,900i' );
 }
-
-wp_enqueue_style( 'montserrat', 'https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i,900,900i' );
 
 ?>
