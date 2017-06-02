c1c431385f383bf1efb9705ef47261830d963c2c
Brandyn Burbank <brandyn@brandynburbank.com>
2017-06-02 16:02:07 -0600

    hide banner container on empty children set.

--- search-layout.js
+++ search-layout.js
@@ -1,5 +1,24 @@
-var announceElement;
+/* 
+** Script for search and layout implementation in White Elegance Child Theme
+** By: Brandyn Burbank for Studio Element (http://studio-element.com/)
+** Last Update: 2 June 2017
+*/
 
+
+//create announcement banner in container
+
+function listAnnouncements(container, announceElement) {
+    (function($){
+        $(container).html(announceElement);
+
+        if ($(container + " .cg-show-announcements").children().length <= 0) {
+            $(container).hide();
+        }
+    })(jQuery);
+}
+
+
+//Pageload scripts
 (function($){
     $(document).ready(function(){
         var searchBox = $('.el-hidden-search');
@@ -9,16 +28,11 @@ var announceElement;
             searchBox.slideToggle();
         });
 
-        announceElement = $('.cg-show-announcements').wrap('<p/>').parent().html();
-
+        //strip theme default announcement banner
+        var announceElement = $('.cg-show-announcements').wrap('<p/>').parent().html();
         $('.cg-show-announcements').parent().remove();
 
-        listAnnouncements("#announce_container");
+        listAnnouncements("#announce_container", announceElement);
     });
 })(jQuery);
 
-function listAnnouncements(container) {
-    (function($){
-        $(container).html(announceElement);
-    })(jQuery);
-}
