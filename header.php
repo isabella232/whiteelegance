a74a3aff998521602b85a99abae2b3282535ad74
Brandyn Burbank <brandyn@brandynburbank.com>
2018-02-28 12:14:11 -0700

    Added Google Fonts Montserrat to header

--- header.php
+++ header.php
@@ -106,6 +106,7 @@ if ( isset( $cg_options['cg_logo_position'] ) ) {
         } else {
             ?><?php echo get_template_directory_uri(); ?>/apple-touch-icon-precomposed.png<?php } ?>"/>
        <!--[if lte IE 9]><script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js"></script><![endif]-->
+       <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
         <?php wp_head(); ?>
     </head>
     <body id="skrollr-body" <?php body_class(); ?>>
